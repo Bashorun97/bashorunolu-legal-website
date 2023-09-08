f37cf4cee638b90c180ffe04e72fcede53610d51
Damian Akpan <damiakpan@gmail.com>
2023-09-08 01:34:26 +0100

    Implemented hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,53 @@
 import React from "react";
-import {Caveat, Mulish} from "next/font/google"
+import Image from "next/image";
+
+import {Caveat, Mulish} from "next/font/google";
+
+import Logo from "../assets/logo.png";
+import HeroPicture from "../assets/hero-image.png";
 
 const caveat = Caveat({subsets: ["latin"]});
 const mullish = Mulish({subsets: ["latin"]});
 
+
 const Heading = (): JSX.Element => {
   return (
-    <div className="flex flex-col text-white py-4" style={{"height": "72vh", backgroundColor: "#454655E5"}}>
-      <div>Nav Bar</div>
-      <div className="bg-red-200 flex flex-grow">
-        <div className="flex flex-grow">
+    <div className="flex flex-col text-white py-2 px-16 gap-4" style={{backgroundColor: "#454655E5"}}>
+      <div className="flex py-2 justify-center items-center">
+        <Image src={Logo} alt="Logo" height={65} width={65} />
 
-          <div className="flex flex-col">
-            <h6 className={caveat.className} style={{fontSize: "64px", transform: "rotate(-5.96deg)", backgroundColor: "red", lineHeight: "53px", width: "max-content"}}>I am</h6>
-            <div className={mullish.className}>Your Trusted Legal Partner in Lagos</div>
-          </div>
-
+        <div className="flex flex-grow justify-center content-center gap-4">
+          <NavButton active>Home</NavButton>
+          <NavButton><p className="flex gap-1">Services <ChevronDown /></p></NavButton>
+          <NavButton>Contact me</NavButton>
+          <NavButton>Payments</NavButton>
         </div>
 
-        <div className="flex flex-grow">Hello</div>
+        <div>call: <span className="text-lg">(234) 01234567890</span></div>
+      </div>
+
+      <div className="flex flex-grow">
+        <div className="flex flex-col w-1/2 gap-6 justify-center">
+          <div>
+            <h6 className={caveat.className + " " + "text-primary"} style={{fontSize: "64px", transform: "rotate(-7.96deg)", lineHeight: "50px", width: "max-content", marginBottom: "-15px", marginLeft: "0"}}>I am</h6>
+            <p className={mullish.className + " " + "text-6xl font-extrabold"}>
+              Your <span className="text-secondary">Trusted</span> Legal Partner in Lagos
+            </p>
+          </div>
+
+          <p>
+            I am dedicated to simplifying legal complexities.
+            Our team of experts is here to guide you through every step,
+            providing efficient and cost-effective solutions to your legal needs.
+          </p>
+
+          <button className="bg-white text-black w-fit px-8 py-3 rounded-md">Contact me</button>
+        </div>
+
+        <div className="flex w-1/2 justify-end">
+          <Image priority src={HeroPicture} alt="Hero picture" height={500} width={470} />
+        </div>
+
       </div>
     </div>
   );
@@ -28,10 +57,32 @@ export default function Home() {
   return (
     <div>
       <Heading />
+      { /*
       <div>Mission</div>
       <div>Revise and Review</div>
       <div>FAQ</div>
       <div>Footer</div>
+      */ }
     </div>
   );
 }
+
+interface NavButtonProps {
+  active?: boolean;
+  children?: React.ReactNode;
+}
+
+const NavButton = ({active = false, children}: NavButtonProps): JSX.Element => {
+  return (
+    <button className={`${active ? "text-primary" : ""}`}>{children}</button>
+  );
+};
+
+
+const ChevronDown = (): JSX.Element => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+    </svg>
+  );
+};
