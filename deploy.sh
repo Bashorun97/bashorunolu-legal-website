898c4e948c70d6222307bccf2cf6320c50d78b4f
Damian Akpan <damiakpan@gmail.com>
2023-09-13 04:06:25 +0100

    Changed deployment to use gcloud cli. Disabled cloud run pinning

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Get the git commit hash
+GIT_COMMIT=$(git rev-parse HEAD)
+
+# Build the docker image
+gcloud builds \
+   submit \
+   --tag=us-central1-docker.pkg.dev/bashorunolu-website-staging/services/website:$GIT_COMMIT
+
+# Deploy the docker image to Cloud Run
+gcloud run deploy website \
+  --image=us-central1-docker.pkg.dev/bashorunolu-website-staging/services/website:$GIT_COMMIT \
+  --platform=managed \
+  --region=us-central1 \
+  --allow-unauthenticated \
+  --memory=512Mi \
+  --cpu=1 \
+  --port=8080 \
+  --min-instances=0 \
+  --max-instances=3 \
+  --timeout=300 \
+  --concurrency=80 \
+  --set-env-vars=NODE_ENV=production \
+  --set-env-vars=PORT=8080
+
+# Deploy firebase hosting
+firebase deploy --only hosting
