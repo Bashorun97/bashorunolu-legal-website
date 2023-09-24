8ee9e1cc3d524a92dab7a7478c1934d70ac0c9a7
Damian Akpan <damiakpan@gmail.com>
2023-09-24 16:52:48 +0100

    Fixed issues with styling of blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,26 +9,18 @@ const Page = () => {
   return (
     <div>
       <div>
-       
         <Navbar />
       </div>
-      <div className="flex px-20 gap-2" style={{ height: "86vh" }}>
-        <div className="bg-blue-400 flex-grow h-full">
-        <div className="flex flex-col cursor-pointer h-full bg-cardBackground overflow-hidden rounded-lg">
-        <div className="flex-grow bg-red-400 "></div>
-        <div className="flex flex-col gap-1 p-4">
-          <h3 className="font-semibold text-lg">Mask Pencil Vertical</h3>
-          <p>
-            Proofreading is the final stage of the editing process,
-            focusing on surface errors such as misspellings and mistakes
-            in grammar and punctuation.
-          </p>
-          <p className="pt-2">17/09/2023</p>
-        </div>
-      </div>
+      <div className="flex px-20 gap-3">
+        <div className="flex-grow min-h-fit">
+          <Postcard
+            title="card"
+            description="post cards"
+            onClick={() => console.log("these are the cards ")}
+          />
         </div>
 
-        <div className=" w-80 flex flex-col gap-2">
+        <div className="flex flex-col gap-3" style={{width: "22rem"}}>
           <Postcard
             title="card"
             description="post cards"
--- src/app/components/Postcard.tsx
+++ src/app/components/Postcard.tsx
@@ -1,30 +1,25 @@
 import React from "react";
 
+interface PostCardProps {
+  title: string;
+  description: string;
+  onClick: () => void;
+}
 
-
-
-
-
-interface PostProps {
-    title: string;
-    description: string;
-    onClick: () => void;
-  }
-  
-  const Post = ({onClick}: PostProps): JSX.Element => {
-    return (
-      <div onClick={onClick} className="flex flex-col cursor-pointer h-96 bg-cardBackground overflow-hidden rounded-lg">
-        <div className=" bg-red-400 pb-40"></div>
-        <div className="flex flex-col gap-1 p-4">
-          <h3 className="font-semibold text-lg">Mask Pencil Vertical</h3>
-          <p>
-            Proofreading is the final stage of the editing process,
-            focusing on surface errors such as misspellings and mistakes
-            in grammar and punctuation.
-          </p>
-          <p className="pt-2">17/09/2023</p>
-        </div>
+const PostCard = ({onClick}: PostCardProps): JSX.Element => {
+  return (
+    <div onClick={onClick} className="flex flex-col cursor-pointer h-full bg-cardBackground overflow-hidden rounded-lg">
+      <div className="bg-red-400 flex-grow" style={{minHeight: "9rem"}}></div>
+      <div className="flex flex-col gap-1 p-3">
+        <h3 className="font-semibold text-md">Mask Pencil Vertical</h3>
+        <p className="text-sm">
+          Proofreading is the final stage of the editing process,
+          focusing on surface errors such as misspellings and mistakes
+          in grammar and punctuation.
+        </p>
+        <p className="pt-2 text-sm text-backgroundBlack">17/09/2023</p>
       </div>
-    );
-  }
-  export default Post;
+    </div>
+  );
+}
+export default PostCard;
