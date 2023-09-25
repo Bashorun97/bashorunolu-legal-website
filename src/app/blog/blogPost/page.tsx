8426eaeaaa0d860096b186950a8c0aa1d234983e
Damian Akpan <damiakpan@gmail.com>
2023-09-25 20:08:26 +0100

    Fixed linting issue

--- src/app/blog/blogPost/page.tsx
+++ src/app/blog/blogPost/page.tsx
@@ -23,7 +23,7 @@ const Effctive = () => {
             Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to
              forswear forever. <br/> <br/>
 
-            Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely 
+            Not so fast, I&apos;d say, there are some redeeming factors in favor of greeking text, as its use is merely 
             the symptom of a worse problem to take into consideration.<br/> <br/>
 
             The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint 
@@ -36,7 +36,7 @@ const Effctive = () => {
         <div className='px-80 py-14'>
           <p>The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may slap on your face to impress the new 
             boss is your business. But what about your daily bread? Not so fast, 
-            I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse
+            I&apos;d say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse
             problem to take into consideration.<br/> <br/>
 
             Design comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain
@@ -50,11 +50,11 @@ const Effctive = () => {
              in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever. <br/> <br/>
 
             The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may 
-            slap on your face to impress the new boss is your business. But what about your daily bread? Not so fast, I'd say, 
+            slap on your face to impress the new boss is your business. But what about your daily bread? Not so fast, I&apos;d say, 
             there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to 
             take into consideration.<br/> <br/>
 
-            Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom
+            Not so fast, I&apos;d say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom
              of a worse problem to take into consideration.<br/> <br/>
 
             Design comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities 
