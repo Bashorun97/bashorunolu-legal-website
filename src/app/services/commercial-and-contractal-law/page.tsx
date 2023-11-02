e9ae07b07dc7b223dc7da03f8fc1ed770756c52e
Uchechi Ugwari <ugwariuchechi2020@gmail.com>
2023-11-02 10:46:35 +0100

    made fixes to services

--- src/app/services/commercial-and-contractal-law/page.tsx
+++ src/app/services/commercial-and-contractal-law/page.tsx
@@ -20,11 +20,139 @@ const CommercialAndContractalLaw = () => {
           headingText="commercial and contractual law"
           faqItems={[
             {
-              title: "Hello",
+              title: "Drafting and Evaluating Hire Purchase Agreements",
               body: (
-                <div>Hello</div>
-              )
-            }
+                <div className="flex flex-col gap-4">
+                  <p>
+                    We offer a comprehensive range of legal services spanning
+                    multiple practice areas. In Property Law Practice, we
+                    provide services including initial inquiry and assessment,
+                    title deduction and investigation, final conveyance
+                    processes, and post-completion procedures. We also handle
+                    public land acquisition, lease and tenancy matters,
+                    mortgages, and charges, offering guidance on mortgages,
+                    property valuation, and more. In addition, we specialize in
+                    wills, codicils, and probate practice, assisting with estate
+                    law administration. Our Corporate Law Practice covers
+                    company promotion, incorporation, investment agreements,
+                    corporate secretarial services, and corporate
+                    reorganization, including mergers and acquisitions. Lastly,
+                    in Commercial and Contractual Law, we excel in drafting
+                    agreements, copyright, industrial property law, trademark
+                    matters, employment law consultations, and contract
+                    establishment and enforcement.r a hassle-free land sale
+                    experience.
+                  </p>
+
+                  <div className="flex gap-2">
+                    <Link
+                      href="/book-appointment"
+                      className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                    >
+                      BOOK AN APPOINTMENT
+                    </Link>
+                    <Link
+                      href="/contact-us"
+                      className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                    >
+                      CONTACT US
+                    </Link>
+                  </div>
+                </div>
+              ),
+            },
+            {
+              title: "Profound Knowledge of Copyright Law",
+              body: (
+                <div className="flex flex-col gap-4">
+                  <p></p>
+
+                  <div className="flex gap-2">
+                    <Link
+                      href="/book-appointment"
+                      className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                    >
+                      BOOK AN APPOINTMENT
+                    </Link>
+                    <Link
+                      href="/contact-us"
+                      className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                    >
+                      CONTACT US
+                    </Link>
+                  </div>
+                </div>
+              ),
+            },
+            {
+              title: "Skilful Handling of Trademark Law Matters",
+              body: (
+                <div className="flex flex-col gap-4">
+                  <p></p>
+
+                  <div className="flex gap-2">
+                    <Link
+                      href="/book-appointment"
+                      className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                    >
+                      BOOK AN APPOINTMENT
+                    </Link>
+                    <Link
+                      href="/contact-us"
+                      className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                    >
+                      CONTACT US
+                    </Link>
+                  </div>
+                </div>
+              ),
+            },
+            {
+              title: "Consultation for Employment Law Matter",
+              body: (
+                <div className="flex flex-col gap-4">
+                  <p></p>
+
+                  <div className="flex gap-2">
+                    <Link
+                      href="/book-appointment"
+                      className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                    >
+                      BOOK AN APPOINTMENT
+                    </Link>
+                    <Link
+                      href="/contact-us"
+                      className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                    >
+                      CONTACT US
+                    </Link>
+                  </div>
+                </div>
+              ),
+            },
+            {
+              title: "Establishing and Enforcing Legally Sound Contracts",
+              body: (
+                <div className="flex flex-col gap-4">
+                  <p></p>
+
+                  <div className="flex gap-2">
+                    <Link
+                      href="/book-appointment"
+                      className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                    >
+                      BOOK AN APPOINTMENT
+                    </Link>
+                    <Link
+                      href="/contact-us"
+                      className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                    >
+                      CONTACT US
+                    </Link>
+                  </div>
+                </div>
+              ),
+            },
           ]}
         />
       </div>
@@ -33,223 +161,3 @@ const CommercialAndContractalLaw = () => {
 };
 
 export default CommercialAndContractalLaw;
-
-
-{/* <div className="absolute w-full" style={{ top: "50vh" }}>
-<div className="flex flex-col gap-8">
-  <div className="flex flex-col rounded-lg bg-white gap-4 mx-2 md:mx-16 p-4">
-    <Accordion
-      open
-      title="Drafting and Evaluating Hire Purchase Agreements"
-      body={
-        <div className="flex flex-col gap-4">
-          <p>
-            We offer a comprehensive range of legal services spanning
-            multiple practice areas. In Property Law Practice, we
-            provide services including initial inquiry and assessment,
-            title deduction and investigation, final conveyance
-            processes, and post-completion procedures. We also handle
-            public land acquisition, lease and tenancy matters,
-            mortgages, and charges, offering guidance on mortgages,
-            property valuation, and more. In addition, we specialize in
-            wills, codicils, and probate practice, assisting with estate
-            law administration. Our Corporate Law Practice covers
-            company promotion, incorporation, investment agreements,
-            corporate secretarial services, and corporate
-            reorganization, including mergers and acquisitions. Lastly,
-            in Commercial and Contractual Law, we excel in drafting
-            agreements, copyright, industrial property law, trademark
-            matters, employment law consultations, and contract
-            establishment and enforcement.r a hassle-free land sale
-            experience.
-          </p>
-
-          <div className="flex gap-2">
-            <Link
-              href="/book-appointment"
-              className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
-            >
-              BOOK AN APPOINTMENT
-            </Link>
-            <Link
-              href="/contact-us"
-              className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
-            >
-              CONTACT US
-            </Link>
-          </div>
-        </div>
-      }
-    />
-
-    <Accordion
-      open
-      title="Profound Knowledge of Copyright Law"
-      body={
-        <div className="flex flex-col gap-4">
-          <p></p>
-
-          <div className="flex gap-2">
-            <Link
-              href="/book-appointment"
-              className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
-            >
-              BOOK AN APPOINTMENT
-            </Link>
-            <Link
-              href="/contact-us"
-              className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
-            >
-              CONTACT US
-            </Link>
-          </div>
-        </div>
-      }
-    />
-    <Accordion
-      open
-      title="Focused Competence in Industrial Property Law"
-      body={
-        <div className="flex flex-col gap-4">
-          <p></p>
-
-          <div className="flex gap-2">
-            <Link
-              href="/book-appointment"
-              className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
-            >
-              BOOK AN APPOINTMENT
-            </Link>
-            <Link
-              href="/contact-us"
-              className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
-            >
-              CONTACT US
-            </Link>
-          </div>
-        </div>
-      }
-    />
-
-    <Accordion
-      open
-      title="Skilful Handling of Trademark Law Matters"
-      body={
-        <div className="flex flex-col gap-4">
-          <p></p>
-
-          <div className="flex gap-2">
-            <Link
-              href="/book-appointment"
-              className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
-            >
-              BOOK AN APPOINTMENT
-            </Link>
-            <Link
-              href="/contact-us"
-              className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
-            >
-              CONTACT US
-            </Link>
-          </div>
-        </div>
-      }
-    />
-    <Accordion
-      open
-      title="Consultation for Employment Law Matter"
-      body={
-        <div className="flex flex-col gap-4">
-          <p></p>
-
-          <div className="flex gap-2">
-            <Link
-              href="/book-appointment"
-              className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
-            >
-              BOOK AN APPOINTMENT
-            </Link>
-            <Link
-              href="/contact-us"
-              className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
-            >
-              CONTACT US
-            </Link>
-          </div>
-        </div>
-      }
-    />
-    <Accordion
-      open
-      title="Establishing and Enforcing Legally Sound Contracts"
-      body={
-        <div className="flex flex-col gap-4">
-          <p></p>
-
-          <div className="flex gap-2">
-            <Link
-              href="/book-appointment"
-              className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
-            >
-              BOOK AN APPOINTMENT
-            </Link>
-            <Link
-              href="/contact-us"
-              className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
-            >
-              CONTACT US
-            </Link>
-          </div>
-        </div>
-      }
-    />
-  </div>
-</div>
-</div>
-<div>
-<div className="flex flex-col gap-4 mt-[28rem] mb-28 px-20">
-  <h4 className="text-2xl font-bold">Related Posts</h4>
-  <div className="grid grid-cols-3 gap-4">
-    <PostCard
-      image={Blog2}
-      title="cards"
-      description="post cards"
-      route="/blog/effective-subtract-fill"
-    />
-    <PostCard
-      image={Blog2}
-      title="card"
-      description="post cards"
-      route="/blog/effective-subtract-fill"
-    />
-    <PostCard
-      image={Blog2}
-      title="card"
-      description="post cards"
-      route="/blog/effective-subtract-fill"
-    />
-    <PostCard
-      image={Blog2}
-      title="card"
-      description="post cards"
-      route="/blog/effective-subtract-fill"
-    />
-    <PostCard
-      image={Blog2}
-      title="card"
-      description="post cards"
-      route="/blog/effective-subtract-fill"
-    />
-    <PostCard
-      image={Blog2}
-      title="card"
-      description="post cards"
-      route="/blog/effective-subtract-fill"
-    />
-  </div>
-</div>
-
-<div>
-  <Footer />
-</div>
-</div> */}
--- src/app/services/property-law-practice/page.tsx
+++ src/app/services/property-law-practice/page.tsx
@@ -24,11 +24,158 @@ const PropertyLawPractice = () => {
         headingText="Property Law Practice (Conveyance Practice)"
         faqItems={[
           {
-            title: "Hello",
+            title: "Sales of Land",
             body: (
-              <div>Hello</div>
-            )
-          }
+              <div className="flex flex-col gap-4">
+                <p>
+                  Our service designed to simplify your property transactions.
+                  We handle it all, from the initial inquiry and assessment to
+                  meticulous title deduction and investigation. Our expertise
+                  extends to guiding you through the final conveyance process
+                  seamlessly, ensuring a smooth transition of ownership. We also
+                  provide comprehensive post-completion support to guarantee
+                  your peace of mind. Trust us for a hassle-free land sale
+                  experience.
+                </p>
+                <p className="-mb-5">This service includes:</p>
+                <ul className="list-disc pl-6 py-2">
+                  <li className="">
+                    Initial/Preliminary Inquiry and Assessment
+                  </li>
+                  <li>Title Deduction and Investigation [Search]</li>
+                  <li>Final Conveyance Process</li>
+                  <li>Post Completion process</li>
+                </ul>
+                <div className="flex gap-2">
+                  <Link
+                    href="/book-appointment"
+                    className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                  >
+                    BOOK AN APPOINTMENT
+                  </Link>
+                  <Link
+                    href="/contact-us"
+                    className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                  >
+                    CONTACT US
+                  </Link>
+                </div>
+              </div>
+            ),
+          },
+          {
+            title: "Public land acquisition and excision processing",
+            body: (
+              <div className="flex flex-col gap-4">
+                <div className="flex gap-2">
+                  <Link
+                    href="/book-appointment"
+                    className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                  >
+                    BOOK AN APPOINTMENT
+                  </Link>
+                  <Link
+                    href="/contact-us"
+                    className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                  >
+                    CONTACT US
+                  </Link>
+                </div>
+              </div>
+            ),
+          },
+          {
+            title: "Lease and tenancies",
+            body: (
+              <div className="flex flex-col gap-4">
+                <p></p>
+
+                <div className="flex gap-2">
+                  <Link
+                    href="/book-appointment"
+                    className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                  >
+                    BOOK AN APPOINTMENT
+                  </Link>
+                  <Link
+                    href="/contact-us"
+                    className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                  >
+                    CONTACT US
+                  </Link>
+                </div>
+              </div>
+            ),
+          },
+          {
+            title: "Mortgages and charges",
+            body: (
+              <div className="flex flex-col gap-4">
+                <p></p>
+
+                <div className="flex gap-2">
+                  <Link
+                    href="/book-appointment"
+                    className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                  >
+                    BOOK AN APPOINTMENT
+                  </Link>
+                  <Link
+                    href="/contact-us"
+                    className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                  >
+                    CONTACT US
+                  </Link>
+                </div>
+              </div>
+            ),
+          },
+          {
+            title: "Wills and codicil",
+            body: (
+              <div className="flex flex-col gap-4">
+                <p></p>
+
+                <div className="flex gap-2">
+                  <Link
+                    href="/book-appointment"
+                    className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                  >
+                    BOOK AN APPOINTMENT
+                  </Link>
+                  <Link
+                    href="/contact-us"
+                    className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                  >
+                    CONTACT US
+                  </Link>
+                </div>
+              </div>
+            ),
+          },
+          {
+            title: "Probate practice",
+            body: (
+              <div className="flex flex-col gap-4">
+                <p></p>
+
+                <div className="flex gap-2">
+                  <Link
+                    href="/book-appointment"
+                    className="bg-blueE text-white px-5 py-3 rounded-md hover:bg-gray-800"
+                  >
+                    BOOK AN APPOINTMENT
+                  </Link>
+                  <Link
+                    href="/contact-us"
+                    className="border-2 border-blueE px-5 py-3 rounded-md hover:bg-gray-100"
+                  >
+                    CONTACT US
+                  </Link>
+                </div>
+              </div>
+            ),
+          },
         ]}
       />
     </main>
